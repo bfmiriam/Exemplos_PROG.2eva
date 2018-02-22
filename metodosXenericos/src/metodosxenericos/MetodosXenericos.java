b3927c1140b7d5fc72bc589efaadbed127bf4331
mbacelofernandez <mbacelofernandez@victoria12.danielcastelao.org>
2018-02-22 13:59:37 +0100

    Exemplo metodos xenericos

---
+++ metodosXenericos/src/metodosxenericos/MetodosXenericos.java
@@ -0,0 +1,36 @@
+package metodosxenericos;
+
+import java.util.ArrayList;
+
+public class MetodosXenericos {
+
+    public static void main(String[] args) {
+        ArrayList<Pantalla> lisPan = new ArrayList<>();
+        ArrayList<Mouse> lisRato = new ArrayList<>();
+        ArrayList<String> lisNomes = new ArrayList<>();
+
+        // Metodos obx = new Metodos();
+        lisPan.add(new Pantalla("samsung", 17f));
+        lisPan.add(new Pantalla("asus", 17.5f));
+        lisPan.add(new Pantalla("samsung", 20f));
+        lisRato.add(new Mouse("inalambrico"));
+        lisRato.add(new Mouse("con cable"));
+        lisNomes.add("Noa");
+        lisNomes.add("Alba");
+        lisNomes.add("Manuel");
+        lisNomes.add("Natalia");
+        lisNomes.add("Sara");
+
+        Metodos.amosar(lisPan);
+        Metodos.amosar(lisRato);
+        Metodos.amosar(lisNomes);
+//      
+//      Metodos.eliminarPosicion(lisPan);
+//      Metodos.amosar(lisPan);
+
+        Metodos.eliminarElemento(lisPan);
+        Metodos.amosar(lisPan);
+
+    }
+
+}
---
+++ metodosXenericos/src/metodosxenericos/Mouse.java
@@ -0,0 +1,27 @@
+package metodosxenericos;
+
+public class Mouse {
+
+    private String tipo;
+
+    public Mouse() {
+    }
+
+    public Mouse(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    @Override
+    public String toString() {
+        return "Mouse => " + "tipo: " + tipo;
+    }
+
+}
